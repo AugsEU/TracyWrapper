55d7a1a36719afd68ec457fb91f94e9f4f62f41e
adahlkvist <AugustDahlkvist@gmail.com>
2024-11-21 23:48:32 +0100

    Run code gen again

---
+++ TracyWrapperStubs/ProfileScope.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+namespace TracyWrapper 
+{
+
+	public class ProfileScope : IDisposable
+	{
+		/// <summary> This method is a stub that does nothing. </summary>
+		public ProfileScope(string name, uint color = ZoneC.DEFAULT, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string function = "", [CallerFilePath] string sourceFile = "") { }
+
+		/// <summary> This method is a stub that does nothing. </summary>
+		public ProfileScope(string name, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string function = "", [CallerFilePath] string sourceFile = "") { }
+
+
+		/// <summary> This method is a stub that does nothing. </summary>
+		public void Dispose() { }
+
+	}
+}
--- TracyWrapperStubs/Profiler.cs
+++ TracyWrapperStubs/Profiler.cs
@@ -1,10 +1,13 @@
-using System.Drawing;
 using System.Runtime.CompilerServices;
 namespace TracyWrapper 
 {
 
 	public static class Profiler
 	{
+
+		/// <summary> This method is a stub that does nothing. </summary>
+		public static void InitThread(string? threadName = null) { }
+
 		/// <summary> This method is a stub that does nothing. </summary>
 		public static void SetEnabled(bool enabled) { }
 
@@ -12,23 +15,10 @@ namespace TracyWrapper
 		public static void HeartBeat(string name = "Frame") { }
 
 		/// <summary> This method is a stub that does nothing. </summary>
-		public static void PushProfileZone(string name, Color? color = null, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string function = "", [CallerFilePath] string sourceFile = "") { }
+		public static void PushProfileZone(string name, uint color = ZoneC.DEFAULT, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string function = "", [CallerFilePath] string sourceFile = "") { }
 
 		/// <summary> This method is a stub that does nothing. </summary>
 		public static void PopProfileZone() { }
-
-	}
-
-	public class ProfileScope : IDisposable
-	{
-		/// <summary> This method is a stub that does nothing. </summary>
-		public ProfileScope(string name, Color color, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string function = "", [CallerFilePath] string sourceFile = "") { }
-
-		/// <summary> This method is a stub that does nothing. </summary>
-		public ProfileScope(string name, [CallerLineNumber] int lineNumber = 0, [CallerMemberName] string function = "", [CallerFilePath] string sourceFile = "") { }
-
-		/// <summary> This method is a stub that does nothing. </summary>
-		public void Dispose() { }
 
 	}
 }
